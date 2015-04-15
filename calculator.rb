c9aa08e3f77b7b21101e4f6eb090767f38c2cf43
glowf <glorie_kristine@yahoo.com>
2015-04-15 19:24:01 +0800

    add improvements to code

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # calculator will accept both float and integer, reject inputs with commas
 # and more than one periods
 def check_input(input)
-  input_valid = !(input.count(".") > 1 || input.count(",")>0)
+  input_valid = !(input.count(".") > 1 || input.count(",") > 0)
   input_float = input.to_f != 0.0 || input.to_s == "0" || input.to_s == "0.0"
   input_valid && input_float
 end
@@ -12,7 +12,7 @@ def check_operator(operator)
 end
 
 # re-start calculator?
-def continue
+def restart
   puts "\nDo you want to go again? y/n"
   answer = gets.chomp
   answer.downcase == "y"
@@ -30,7 +30,7 @@ def compute(numbers)
     end
     result = numbers.inject(operator.to_sym)
     puts "##### Result is #{result} #####"
-    start_calculator if continue
+    start_calculator if restart
   else
     puts "Invalid Operator!"
     compute(numbers)
@@ -39,35 +39,29 @@ end
 
 # start the calculator, stop the loop once max input is reached
 def start_calculator
-  question  = "Give me your first number:"
-  numbers   = []
+  question   = "Give me your first number:"
+  numbers    = []
   max_inputs = 2
 
   system "clear"
-  puts ""
-  puts "-----------------------------"
+  puts "\n-----------------------------"
   puts "------ RUBY CALCULATOR ------"
-  puts "-----------------------------"
-  puts ""
+  puts "-----------------------------\n\n"
 
   loop do
     puts question
     answer = gets.chomp
-
     if check_input(answer)
       numbers << answer
-
       if numbers.size == max_inputs
         compute(numbers)
         break
       end
-
       question = "Give me your next number:"
     else
       puts "Oopps, that's not a valid input!"
     end
   end
 end
-
 
 start_calculator
